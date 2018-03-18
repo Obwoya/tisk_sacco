e49438aa6f10c1b2c729b589cdd2274248134d53
Evans Munene <munene.evansk@gmail.com>
2018-03-18 22:23:35 +0300

    [ADD] recent transactions component

---
+++ src/Components/RecentTransactions/__tests__/RecentTransactions.test.js
@@ -0,0 +1,16 @@
+/* global  it, describe, expect */
+import React from "react"
+import { shallow } from "enzyme"
+import RecentTransactions from "../index"
+import styles from "../style.css"
+
+describe("<RecentTransactions/>", () => {
+	it("Must render the wrapper div ", function() {
+		expect(shallow(<div className={styles.recentTransactionsGrid} />).exists())
+	})
+	it("Must all the right number of grids ", function() {
+		const RecentTransactionsComponent = shallow(<RecentTransactions />)
+		expect(shallow(<div />).exists())
+		expect(RecentTransactionsComponent.find("div").length === 3).toBe(true)
+	})
+})
---
+++ src/Components/RecentTransactions/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styles from "./style.css"
+
+const RecentTransactions = () => {
+	return (
+		<div className={styles.recentTransactionsGrid}>
+			<h2>Recent Transactions</h2>
+			<div />
+			<div />
+		</div>
+	)
+}
+
+export default RecentTransactions
