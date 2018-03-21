6830d1848c9a8f02d73029035a2d2ad61e0b5ea7
Evans Munene <munene.evansk@gmail.com>
2018-03-21 12:50:43 +0300

    [ADD] tests for signin and sign up

---
+++ src/Containers/SignIn/__tests__/SignIn.test.js
@@ -0,0 +1,16 @@
+/* global  it, describe, expect */
+import React from "react"
+import { shallow } from "enzyme"
+import SignIn from "../index"
+import styles from "../style.css"
+
+describe("<SignIn/>", () => {
+	it("Must render ", function() {
+		expect(shallow(<SignIn />).exists())
+	})
+	it("Must render the wrapper div ", function() {
+		const SignInPage = shallow(<SignIn />)
+		expect(SignInPage.getElement().props.className).toEqual("signInGrid")
+		expect(SignInPage.getElement().type).toEqual("div")
+	})
+})
---
+++ src/Containers/SignUp/__tests__/SignUp.test.js
@@ -0,0 +1,16 @@
+/* global  it, describe, expect */
+import React from "react"
+import { shallow } from "enzyme"
+import SignUp from "../index"
+import styles from "../style.css"
+
+describe("<SignUp/>", () => {
+	it("Must render ", function() {
+		expect(shallow(<SignUp />).exists())
+	})
+	it("Must render the wrapper div ", function() {
+		const SignUpPage = shallow(<SignUp />)
+		expect(SignUpPage.getElement().props.className).toEqual("signUpGrid")
+		expect(SignUpPage.getElement().type).toEqual("div")
+	})	
+})
