46e050f59922b270674559ec3832aafe6f2d5718
Evans Munene <munene.evansk@gmail.com>
2018-03-21 13:05:53 +0300

    [UPDATE] removed unused style in tests

--- src/Containers/SignIn/__tests__/SignIn.test.js
+++ src/Containers/SignIn/__tests__/SignIn.test.js
@@ -2,7 +2,7 @@
 import React from "react"
 import { shallow } from "enzyme"
 import SignIn from "../index"
-import styles from "../style.css"
+
 
 describe("<SignIn/>", () => {
 	it("Must render ", function() {
--- src/Containers/SignUp/__tests__/SignUp.test.js
+++ src/Containers/SignUp/__tests__/SignUp.test.js
@@ -2,7 +2,7 @@
 import React from "react"
 import { shallow } from "enzyme"
 import SignUp from "../index"
-import styles from "../style.css"
+
 
 describe("<SignUp/>", () => {
 	it("Must render ", function() {
