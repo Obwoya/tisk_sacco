347ddaf7425160685af82d34008b0a1b87906f17
Evans Munene <munene.evansk@gmail.com>
2018-04-04 13:04:10 +0300

    [BUILD]

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,3 +1,5 @@
+importScripts("/build/precache-manifest.a7a6eb3c11654685b5adfdecba199cd1.js", "https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
+
 /* global  importScripts, workbox*/
 
 if ("function" === typeof importScripts) {
@@ -40,5 +42,24 @@ if ("function" === typeof importScripts) {
 				]
 			})
 		)
+
+		workbox.routing.registerRoute(
+			// Cache image files
+			/.*\.(?:ttf|woff|woff2)/,
+			// Use the cache if it's available
+			workbox.strategies.cacheFirst({
+				// Use a custom cache name
+				cacheName: "font-cache",
+				plugins: [
+					new workbox.expiration.Plugin({
+						// Cache only 20 images
+						maxEntries: 20,
+						// Cache for a maximum of a week
+						maxAgeSeconds: 3 * 7 * 24 * 60 * 60
+					})
+				]
+			})
+		)
 	}
 }
+
