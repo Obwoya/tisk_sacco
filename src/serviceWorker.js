7eaf8940947f9fd9487657d501c4e60f31fc6e7e
Evans Munene <munene.evansk@gmail.com>
2018-04-04 11:32:42 +0300

    [ADD] image caching

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,4 @@
+/* global  importScripts, workbox*/
 importScripts(
 	"https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js"
 )
@@ -17,6 +18,22 @@ if (workbox) {
 			cacheName: "css-cache"
 		})
 	)
-} else {
-	console.log("Boo! Workbox didn't load 😬")
+
+	workbox.routing.registerRoute(
+		// Cache image files
+		/.*\.(?:png|jpg|jpeg|svg|gif)/,
+		// Use the cache if it's available
+		workbox.strategies.cacheFirst({
+			// Use a custom cache name
+			cacheName: "image-cache",
+			plugins: [
+				new workbox.expiration.Plugin({
+					// Cache only 20 images
+					maxEntries: 20,
+					// Cache for a maximum of a week
+					maxAgeSeconds: 3 * 7 * 24 * 60 * 60
+				})
+			]
+		})
+	)
 }
