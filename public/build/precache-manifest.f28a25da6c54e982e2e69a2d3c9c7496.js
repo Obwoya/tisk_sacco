8a0d2ef2e1f7b385a031dec1162d2b2f6e49f1d2
Evans Munene <munene.evansk@gmail.com>
2018-04-11 13:26:15 +0300

    [build]

---
+++ public/build/precache-manifest.f28a25da6c54e982e2e69a2d3c9c7496.js
@@ -0,0 +1,63 @@
+self.__precacheManifest = [
+  {
+    "url": "/build/da004d065fc686878821044fe35b3e4d.png"
+  },
+  {
+    "url": "/build/icon_96x96.d2be53f882299e1a1dc0deebc17b8aec.png"
+  },
+  {
+    "revision": "3376509c79181202ff26",
+    "url": "/build/vendor.js"
+  },
+  {
+    "url": "/build/f2a4eeab3c5e6b50c5d9f1e65b36518a.png"
+  },
+  {
+    "revision": "c01237f24c8abc7ce5dd",
+    "url": "/build/app.js"
+  },
+  {
+    "url": "/build/e6bc2356bea135c617f678212d50bd21.woff2"
+  },
+  {
+    "url": "/build/icon_128x128.c338f104f02ba0884465f63453d4ed9c.png"
+  },
+  {
+    "url": "/build/icon_192x192.296f42eb83f725d02067492f005eed53.png"
+  },
+  {
+    "url": "/build/icon_256x256.65770b37a650b2400b1e1b33efa66282.png"
+  },
+  {
+    "url": "/build/icon_384x384.db69c2d21f5d3a6a888d4a58b4d2e2ad.png"
+  },
+  {
+    "url": "/build/icon_512x512.38f19580785bc54ca424b9da3c752e4c.png"
+  },
+  {
+    "url": "/build/f4043e726fb5d48655db2f5ecff8d74c.woff2"
+  },
+  {
+    "revision": "c01237f24c8abc7ce5dd",
+    "url": "/build/app.css"
+  },
+  {
+    "url": "/build/a5f882f2d446645d97e76a9050cd6b70.png"
+  },
+  {
+    "url": "/build/9e62d6656fa85d6b068bd08003a04f8d.svg"
+  },
+  {
+    "url": "/build/8e616dc8b404bc0ab0a71063ed6b7148.svg"
+  },
+  {
+    "url": "/build/1a68b2b5f5eced40677e4ead5fb22f9e.woff2"
+  },
+  {
+    "url": "/build/0ec36d183ddd6551242cde75367243a3.gif"
+  },
+  {
+    "revision": "04f686f7a5ae569e3fac38eda6189cfc",
+    "url": "/build/../index.html"
+  }
+];
