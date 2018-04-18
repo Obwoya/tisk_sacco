5da3e770f4ea2d47305dfd149501956545b01174
Evans Munene <munene.evansk@gmail.com>
2018-04-18 11:51:07 +0300

    [BUILD]

--- public/build/precache-manifest.f96249a0f42a73f3042fa6af435bbdff.js
+++ public/build/precache-manifest.f96249a0f42a73f3042fa6af435bbdff.js
@@ -6,18 +6,18 @@ self.__precacheManifest = [
     "url": "/build/icon_96x96.d2be53f882299e1a1dc0deebc17b8aec.png"
   },
   {
-    "revision": "637685eb4e58cd718d56",
+    "revision": "37af4aa66e72e6efac1c",
     "url": "/build/serviceWorker.js"
   },
   {
-    "revision": "0f028c8c7f66fcaab5a6",
+    "revision": "fb383972b0e2a10e386e",
     "url": "/build/vendor.js"
   },
   {
     "url": "/build/f2a4eeab3c5e6b50c5d9f1e65b36518a.png"
   },
   {
-    "revision": "14c3fbea7b054837cb01",
+    "revision": "be9cdb4fb0893d7eae1c",
     "url": "/build/app.js"
   },
   {
@@ -42,7 +42,7 @@ self.__precacheManifest = [
     "url": "/build/f4043e726fb5d48655db2f5ecff8d74c.woff2"
   },
   {
-    "revision": "14c3fbea7b054837cb01",
+    "revision": "be9cdb4fb0893d7eae1c",
     "url": "/build/app.css"
   },
   {
