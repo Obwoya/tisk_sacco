761925733392a0761bd0c7199edddb353e47a587
muneneevans <munene.evansk@gmail.com>
2018-05-11 14:53:00 +0300

    [Update] activate page uses numpad component

--- src/Containers/Activate/index.js
+++ src/Containers/Activate/index.js
@@ -7,97 +7,19 @@ import styles from "./style.css"
 import * as userActions from "../../Store/Users/actions"
 import * as userSelectors from "../../Store/Users/selectors"
 
+import NumPad from "../../Components/NumberPad"
+
 class Activate extends React.Component {
 	constructor(props) {
-		super(props)
-
-		this.state = {
-			noOfCharsInCode: 6
-		}
+		super(props)		
 	}
 
-	onPasteHandler = e => {
-		let text = e.clipboardData.getData("Text")
-
-		for (let i = 0; i < this.state.noOfCharsInCode; i++) {
-			if (!(text.length > i) || !(this.state.noOfCharsInCode > i)) return
-			let inp = document.getElementById(`code_input_${i}`)
-			inp.value = text[i]
-		}
-	}
-
-	focusNext = index => {
-		if (!(this.state.noOfCharsInCode > index + 1)) return
-		let curInp = document.getElementById(`code_input_${index}`)
-
-		if (curInp.value == "") return
-		let inp = document.getElementById(`code_input_${index + 1}`)
-		inp.focus()
-	}
-
-	onKeyUpHandler = e => {
-		let keyCode = e.keyCode
-		switch (keyCode) {
-			case 37:
-			case 39:
-				var rx = /^code_input_(.*)$/
-				let dir = keyCode - 38
-				let id = parseInt(rx.exec(e.target.id)[1]) + dir
-				let inp = document.getElementById(`code_input_${id}`)
-				if (inp == null) break
-				inp.focus()
-			default:
-				break
-		}
-	}
-
-	onFocusHandler = e => {
-		e.target.select()
-	}
-
-	onSubmitHandler = e => {
-		e.preventDefault()
-		let code = ""
-
-		for (let i = 0; i < this.state.noOfCharsInCode; i++) {
-			let inp = document.getElementById(`code_input_${i}`)
-			code = code + inp.value
-		}
-
-		this.props.userActions.activateUser({ token: code })
-		// The actual code
-		// console.log(code)
+	onSubmitHandler = token=> {
+		this.props.userActions.activateUser({ token: token.number })
 	}
 	render() {
-		let inputs = []
-		for (let i = 0; i < this.state.noOfCharsInCode; i++) {
-			inputs.push(
-				<input
-					type="text"
-					key={i}
-					id={`code_input_${i}`}
-					onKeyUp={this.onKeyUpHandler}
-					onFocus={this.onFocusHandler}
-					required
-					onPaste={this.onPasteHandler}
-					maxLength="1"
-					onChange={() => this.focusNext(i)}
-				/>
-			)
-		}
-		return (
-			<div className={styles.activatePage}>
-				<div className={styles.topBar}>
-					<span className={styles.arrowBack}>&#8592;</span>
-					Insert Activation Code
-				</div>
-				<form onSubmit={this.onSubmitHandler}>
-					<div className={styles.pageContent}>
-						<div className={styles.activateCodeWrapper}>{inputs}</div>
-					</div>
-					<input type="submit" className={styles.depositNext} value="Next" />
-				</form>
-			</div>
+		return(
+			<NumPad size={6} submitAction={this.onSubmitHandler.bind(this)} title="User Activation Code" description={"An activation code has been send to your "}/> 
 		)
 	}
 }
--- src/Store/Users/services.js
+++ src/Store/Users/services.js
@@ -2,8 +2,8 @@ export default class UsersService {
 	static get host() {
 		delete UsersService.host
 		// return (UsersService.host = "https://demo-tiskserver.herokuapp.com")
-		// return (UsersService.host = "http://localhost:8000")
-		return (UsersService.host = "http://188.166.248.71")
+		return (UsersService.host = "http://localhost:8000")
+		// return (UsersService.host = "http://188.166.248.71")
 	}
 
 	static getToken(user) {
