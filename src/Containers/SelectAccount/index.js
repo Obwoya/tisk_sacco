7bf1bf1d6519b3807879e4ae78ee922484f23cb8
muneneevans <munene.evansk@gmail.com>
2018-05-21 23:29:29 +0300

    [ADD] Accordion

--- src/Containers/SelectAccount/index.js
+++ src/Containers/SelectAccount/index.js
@@ -1,105 +1,23 @@
 import React, { Component } from "react"
 
-import styles from "./style.css"
+import  "./style.css"
 
-import Button from "../../Components/Button"
 
+import AccordionComponent from "../../Components/Accordion"
 class SelectAccount extends Component {
 	render() {
-		let { parent, child, primaryText, secondaryText, buttonContainer } = styles
+		
+		const data = {
+			do: "a deer, a female deer ",
+			re: "a drop of golden sun ",
+			me: "a name, I call myself ",
+			fa: "a long long way to run "
+		}
 		return (
-			<div className={parent}>
-				<div className={child}>
-					<div className={buttonContainer}>
-						<Button
-							children={
-								<div >
-									<div>
-										<h3 className={primaryText}>INVESTOR</h3>
-									</div>
-									<div>
-										<p className={secondaryText}>Terabyte</p>
-									</div>
-								</div>
-							}
-							backgroundColor={"#b32017"}
-							foregroundColor={"#ffffff"}
-							raised={true}
-							clickAction={() => {}}
-						/>
-					</div>					
-					<div className={buttonContainer}>
-						<Button
-							children={
-								<div >
-									<div>
-										<h3 className={primaryText}>BUSINESS</h3>
-									</div>
-									<div>
-										<p className={secondaryText}>Gigabyte</p>
-									</div>
-								</div>
-							}
-							backgroundColor={"#b32017"}
-							foregroundColor={"#ffffff"}
-							raised={true}
-							clickAction={() => {}}
-						/>
-					</div>					
-					<div className={buttonContainer}>
-						<Button
-							children={
-								<div >
-									<div>
-										<h3 className={primaryText}>PROFESSIONAL</h3>
-									</div>
-									<div>
-										<p className={secondaryText}>Megabyte</p>
-									</div>
-								</div>
-							}
-							backgroundColor={"#b32017"}
-							foregroundColor={"#ffffff"}
-							raised={true}
-							clickAction={() => {}}
-						/>
-					</div>					
-					<div className={buttonContainer}>
-						<Button
-							children={
-								<div >
-									<div>
-										<h3 className={primaryText}>STUDENT</h3>
-									</div>
-									<div>
-										<p className={secondaryText}>Kilobyte</p>
-									</div>
-								</div>
-							}
-							backgroundColor={"#b32017"}
-							foregroundColor={"#ffffff"}
-							raised={true}
-							clickAction={() => {}}
-						/>
-					</div>					
-					<div className={buttonContainer}>
-						<Button
-							children={
-								<div >
-									<div>
-										<h3 className={primaryText}>FUTURES</h3>
-									</div>
-									<div>
-										<p className={secondaryText}>Microbyte</p>
-									</div>
-								</div>
-							}
-							backgroundColor={"#b32017"}
-							foregroundColor={"#ffffff"}
-							raised={true}
-							clickAction={() => {}}
-						/>
-					</div>					
+			<div className="parent">
+				<div className="child">
+					<AccordionComponent  data={data}/>
+					
 				</div>
 			</div>
 		)
