4e1b0a2edd2695689c2fc9698423639c3b9b77f0
muneneevans <munene.evansk@gmail.com>
2018-05-22 00:54:24 +0300

    [BUILD]

--- public/build/precache-manifest.6ea5fc84e3354642cdabae2e235f76fb.js
+++ public/build/precache-manifest.6ea5fc84e3354642cdabae2e235f76fb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "url": "/build/da004d065fc686878821044fe35b3e4d.png"
+    "url": "/build/f2a4eeab3c5e6b50c5d9f1e65b36518a.png"
   },
   {
     "url": "/build/icon_96x96.ebb51083bbd506b690973e4d9f6d0596.png"
   },
   {
-    "revision": "88331596c56fb2780baf",
+    "revision": "9b11d566a2a0ffb9e1ef",
     "url": "/build/serviceWorker.js"
   },
   {
-    "revision": "bb3a50ce4fbfc23076b9",
+    "revision": "e0363b1d2eea3058d00e",
     "url": "/build/vendor.js"
   },
   {
-    "url": "/build/f2a4eeab3c5e6b50c5d9f1e65b36518a.png"
+    "url": "/build/25960fb48bcd268e8e1f3734cf4757c7.svg"
   },
   {
-    "revision": "593867f5170f64cbb360",
+    "revision": "ee5b2c6ce2af82910e62",
     "url": "/build/app.js"
   },
   {
@@ -33,16 +33,28 @@ self.__precacheManifest = [
     "url": "/build/icon_256x256.cfd6d4317a1baa07bb1d676d9f970ce0.png"
   },
   {
+    "url": "/build/da004d065fc686878821044fe35b3e4d.png"
+  },
+  {
     "url": "/build/icon_384x384.abd5ee72db57cb8996ff0b9eadd2559d.png"
   },
   {
     "url": "/build/icon_512x512.3edb0e310052db3aa53d8d835f2dfec5.png"
   },
   {
+    "url": "/build/ce87ca52799bae686235129b79782748.svg"
+  },
+  {
     "url": "/build/f4043e726fb5d48655db2f5ecff8d74c.woff2"
   },
   {
-    "revision": "593867f5170f64cbb360",
+    "url": "/build/d9cadd6641b241f7fa496c3efde3ccc5.svg"
+  },
+  {
+    "url": "/build/f55c32d63f914a507757dba9c66c9b84.svg"
+  },
+  {
+    "revision": "ee5b2c6ce2af82910e62",
     "url": "/build/app.css"
   },
   {
@@ -53,6 +65,9 @@ self.__precacheManifest = [
   },
   {
     "url": "/build/8e616dc8b404bc0ab0a71063ed6b7148.svg"
+  },
+  {
+    "url": "/build/572c24fc87cecca85592fc7c4fedff5d.svg"
   },
   {
     "url": "/build/1a68b2b5f5eced40677e4ead5fb22f9e.woff2"
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/build/precache-manifest.76ef4e59be782faf0268356d2ce3afd6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
+importScripts("/build/precache-manifest.6ea5fc84e3354642cdabae2e235f76fb.js", "https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
 
 /* global  importScripts, workbox*/
 
