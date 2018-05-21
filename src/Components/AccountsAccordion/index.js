530749266db7e0eb617ec0e11046a9fa20f1e516
muneneevans <munene.evansk@gmail.com>
2018-05-22 00:21:40 +0300

    []UPDATE] Accordion

--- src/Components/AccountsAccordion/index.js
+++ src/Components/AccountsAccordion/index.js
@@ -7,8 +7,6 @@ import {
 	AccordionItemBody
 } from "react-accessible-accordion"
 
-import { CSSTransition, TransitionGroup } from "react-transition-group"
-
 // Demo styles, see 'Styles' section below for some notes on use.
 import "react-accessible-accordion/dist/fancy-example.css"
 import "./style.css"
@@ -20,66 +18,64 @@ import baby from "./images/baby.svg"
 import professional from "./images/professional.svg"
 
 const Example = () => (
-	<Accordion>
-		<TransitionGroup>
-			<AccordionItem>
-				<AccordionItemTitle className="accordion__header">
-					<img src={investor} className="accordion__image" />
-					<h3>INVESTOR</h3>
-				</AccordionItemTitle>
-				<AccordionItemBody>
-					<p>Terabyte</p>
-				</AccordionItemBody>
-			</AccordionItem>
-			<AccordionItem>
-				<AccordionItemTitle
-					className="accordion__header"
-					style={{ background: "#fbc531" }}
-				>
-					<img src={business} className="accordion__image" />
-					<h3>BUSINESS</h3>
-				</AccordionItemTitle>
-				<AccordionItemBody>
-					<p>Gigabyte</p>
-				</AccordionItemBody>
-			</AccordionItem>
-			<AccordionItem>
-				<AccordionItemTitle
-					className="accordion__header"
-					style={{ background: "#353b48" }}
-				>
-					<img src={professional} className="accordion__image" />
-					<h3>PROFESSIONAL</h3>
-				</AccordionItemTitle>
-				<AccordionItemBody>
-					<p>Megabyte</p>
-				</AccordionItemBody>
-			</AccordionItem>
-			<AccordionItem>
-				<AccordionItemTitle
-					className="accordion__header"
-					style={{ background: "#8c7ae6" }}
-				>
-					<img src={student} className="accordion__image" />
-					<h3>STUDENT</h3>
-				</AccordionItemTitle>
-				<AccordionItemBody>
-					<p>Kilobyte</p>
-				</AccordionItemBody>
-			</AccordionItem>
-			<AccordionItem>
-				<AccordionItemTitle
-					className="accordion__header"
-					style={{ background: "#44bd32" }}
-				>
-					<img src={baby} className="accordion__image" />
-					<h3>FUTURES</h3>
-				</AccordionItemTitle>
-				<AccordionItemBody>
-					<p>Microbyte</p>
-				</AccordionItemBody>
-			</AccordionItem>
-		</TransitionGroup>
+	<Accordion className="accordion">
+		<AccordionItem>
+			<AccordionItemTitle className="accordion__header">
+				<img src={investor} className="accordion__image" />
+				<h3>INVESTOR</h3>
+			</AccordionItemTitle>
+			<AccordionItemBody>
+				<p>Terabyte</p>
+			</AccordionItemBody>
+		</AccordionItem>
+		<AccordionItem>
+			<AccordionItemTitle
+				className="accordion__header"
+				style={{ background: "#fbc531" }}
+			>
+				<img src={business} className="accordion__image" />
+				<h3>BUSINESS</h3>
+			</AccordionItemTitle>
+			<AccordionItemBody>
+				<p>Gigabyte</p>
+			</AccordionItemBody>
+		</AccordionItem>
+		<AccordionItem>
+			<AccordionItemTitle
+				className="accordion__header"
+				style={{ background: "#353b48" }}
+			>
+				<img src={professional} className="accordion__image" />
+				<h3>PROFESSIONAL</h3>
+			</AccordionItemTitle>
+			<AccordionItemBody>
+				<p>Megabyte</p>
+			</AccordionItemBody>
+		</AccordionItem>
+		<AccordionItem>
+			<AccordionItemTitle
+				className="accordion__header"
+				style={{ background: "#8c7ae6" }}
+			>
+				<img src={student} className="accordion__image" />
+				<h3>STUDENT</h3>
+			</AccordionItemTitle>
+			<AccordionItemBody>
+				<p>Kilobyte</p>
+			</AccordionItemBody>
+		</AccordionItem>
+		<AccordionItem>
+			<AccordionItemTitle
+				className="accordion__header"
+				style={{ background: "#44bd32" }}
+			>
+				<img src={baby} className="accordion__image" />
+				<h3>FUTURES</h3>
+			</AccordionItemTitle>
+			<AccordionItemBody>
+				<p>Microbyte</p>
+			</AccordionItemBody>
+		</AccordionItem>
 	</Accordion>
 )
 
