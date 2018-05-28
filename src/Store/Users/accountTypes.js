231e1bc2864c3c09967fae5f7287e552cd0ea2dc
muneneevans <munene.evansk@gmail.com>
2018-05-28 11:42:50 +0300

    [UPDATE] updated account types

--- src/Store/Users/accountTypes.js
+++ src/Store/Users/accountTypes.js
@@ -5,27 +5,26 @@ export const accountTypes = [
 			{
 				id: 6,
 				name: "Futures",
-				monthly_fee: "1000.00",
-				description:
-					"Microbyte membership is designed to provide a savings facility for tech parents to save for their children who have not reached the majority age. Such members shall not have rights to borrow from the society.",
-				registration_fee: "1000.00",
-				share_capital: "2000.00",
+				monthly_fee: null,
+				description: null,
+				registration_fee: null,
+				share_capital: null,
 				active: false,
 				thumbnail: null,
-				color: "#44BD32",
+				color: null,
 				type: "Future",
 				membership_category: "microbyte"
 			}
 		],
 		monthly_fee: "1000.00",
-		display_name: "MicroByte",
+		display_name: "FUTURES",
 		description:
 			"Microbyte membership is designed to provide a savings facility for tech parents to save for their children who have not reached the majority age. Such members shall not have rights to borrow from the society.",
 		registration_fee: "1000.00",
 		share_capital: "2000.00",
 		active: true,
 		thumbnail:
-			"http://tisk.or.ke/media/membership_categories_membershipcategory/7f1c931a-ea08-4802-8e36-8c75983b0a1d.svg",
+			"http://tisk.or.ke/media/membership_categories_membershipcategory/ecfedbfa-3b10-433c-8856-7c078058aaf5.png",
 		color: "#44BD32"
 	},
 	{
@@ -34,27 +33,26 @@ export const accountTypes = [
 			{
 				id: 4,
 				name: "Professional",
-				monthly_fee: "1000.00",
-				description:
-					"Megabyte members shall be salaried or non-salaried ICT practitioners who make their regular remittances to the society through check-off or paid directly to the society’s account.",
-				registration_fee: "1000.00",
-				share_capital: "2000.00",
+				monthly_fee: null,
+				description: null,
+				registration_fee: null,
+				share_capital: null,
 				active: true,
 				thumbnail: null,
-				color: "#353B48",
+				color: null,
 				type: "Individual",
 				membership_category: "megabyte"
 			}
 		],
 		monthly_fee: "1000.00",
-		display_name: "MegaByte",
+		display_name: "PROFESSIONAL",
 		description:
 			"Megabyte members shall be salaried or non-salaried ICT practitioners who make their regular remittances to the society through check-off or paid directly to the society’s account.",
 		registration_fee: "1000.00",
 		share_capital: "2000.00",
 		active: true,
 		thumbnail:
-			"http://tisk.or.ke/media/membership_categories_membershipcategory/de412d3a-4b23-49e1-8a00-e9feadfc256d.svg",
+			"http://tisk.or.ke/media/membership_categories_membershipcategory/e9bbbfb3-e66e-4355-adb7-39330689a025.png",
 		color: "#353B48"
 	},
 	{
@@ -63,27 +61,26 @@ export const accountTypes = [
 			{
 				id: 3,
 				name: "Business",
-				monthly_fee: "10000.00",
-				description:
-					"Gigabyte members shall consists of Groups, Businesses and Corporate entities admitted into membership in accordance to the bylaws.",
-				registration_fee: "25000.00",
-				share_capital: "50000.00",
+				monthly_fee: null,
+				description: null,
+				registration_fee: null,
+				share_capital: null,
 				active: true,
 				thumbnail: null,
-				color: "#FBC531",
+				color: null,
 				type: "Business",
 				membership_category: "gigabyte"
 			}
 		],
 		monthly_fee: "10000.00",
-		display_name: "GigaByte",
+		display_name: "BUSINESS",
 		description:
 			"Gigabyte members shall consists of Groups, Businesses and Corporate entities admitted into membership in accordance to the by laws.",
 		registration_fee: "25000.00",
 		share_capital: "50000.00",
 		active: true,
 		thumbnail:
-			"http://tisk.or.ke/media/membership_categories_membershipcategory/58bfe152-64d9-4521-a60c-ba6b5785e39e.svg",
+			"http://tisk.or.ke/media/membership_categories_membershipcategory/32338758-aae0-4731-bda5-a423f9b30e33.png",
 		color: "#FBC531"
 	},
 	{
@@ -92,70 +89,67 @@ export const accountTypes = [
 			{
 				id: 5,
 				name: "Student",
-				monthly_fee: "1000.00",
-				description:
-					"Kilobyte members shall be students of ICT and tech consumers who will make their regular remittances to the society.",
-				registration_fee: "1000.00",
-				share_capital: "2000.00",
+				monthly_fee: null,
+				description: null,
+				registration_fee: null,
+				share_capital: null,
 				active: true,
 				thumbnail: null,
-				color: "#8C7AE6",
+				color: null,
 				type: "Individual",
 				membership_category: "kilobyte"
 			}
 		],
 		monthly_fee: "1000.00",
-		display_name: "KiloByte",
+		display_name: "STUDENT",
 		description:
 			"Kilobyte members shall be students of ICT and tech consumers who will make their regular remittances to the society.",
 		registration_fee: "1000.00",
 		share_capital: "2000.00",
 		active: true,
 		thumbnail:
-			"http://tisk.or.ke/media/membership_categories_membershipcategory/6fcfa229-ea77-4073-a538-e6b234a40059.svg",
+			"http://tisk.or.ke/media/membership_categories_membershipcategory/66cec988-a817-4464-9c13-931d193a5708.png",
 		color: "#8C7AE6"
 	},
 	{
 		slug: "terabyte",
 		member_types: [
 			{
-				id: 2,
-				name: "Investor Individual",
-				monthly_fee: "15000.00",
-				description:
-					"Terabyte members shall be persons whose interest in the Sacco is to invest through shares and deposits.",
-				registration_fee: "50000.00",
-				share_capital: "950000.00",
+				id: 1,
+				name: "Investor Business",
+				monthly_fee: null,
+				description: null,
+				registration_fee: null,
+				share_capital: null,
 				active: true,
 				thumbnail: null,
-				color: "#B32017",
-				type: "Individual",
+				color: null,
+				type: "Business",
 				membership_category: "terabyte"
 			},
 			{
-				id: 1,
-				name: "Investor Business",
-				monthly_fee: "15000.00",
-				description:
-					"Terabyte members shall be persons whose interest in the Sacco is to invest through shares and deposits.",
-				registration_fee: "50000.00",
-				share_capital: "950000.00",
+				id: 2,
+				name: "Investor Individual",
+				monthly_fee: null,
+				description: null,
+				registration_fee: null,
+				share_capital: null,
 				active: true,
 				thumbnail: null,
-				color: "#B32017",
-				type: "Business",
+				color: null,
+				type: "Individual",
 				membership_category: "terabyte"
 			}
 		],
 		monthly_fee: "15000.00",
-		display_name: "Terabyte",
+		display_name: "INVESTOR",
 		description:
 			"Terabyte members shall be persons whose interest in the Sacco is to invest through shares and deposits.",
 		registration_fee: "50000.00",
 		share_capital: "950000.00",
 		active: true,
 		thumbnail:
-			"http://tisk.or.ke/media/membership_categories_membershipcategory/468e4293-2860-40f8-a3ce-e233b552f990.png",
+			"http://tisk.or.ke/media/membership_categories_membershipcategory/a0efed30-3ab1-414c-956d-23e47267ec31.png",
 		color: "#B32017"
 	}
 ]
