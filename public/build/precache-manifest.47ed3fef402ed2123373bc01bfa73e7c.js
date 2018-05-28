258c8987ef3598b41370593cc2d45ef0aac3a733
muneneevans <munene.evansk@gmail.com>
2018-05-28 16:43:28 +0300

    [BUILD]

--- public/build/precache-manifest.47ed3fef402ed2123373bc01bfa73e7c.js
+++ public/build/precache-manifest.47ed3fef402ed2123373bc01bfa73e7c.js
@@ -17,7 +17,7 @@ self.__precacheManifest = [
     "url": "/build/f2a4eeab3c5e6b50c5d9f1e65b36518a.png"
   },
   {
-    "revision": "3b434e9e4e37b0d656fc",
+    "revision": "da2a34934418b2092413",
     "url": "/build/app.js"
   },
   {
@@ -45,7 +45,7 @@ self.__precacheManifest = [
     "url": "/build/ce87ca52799bae686235129b79782748.svg"
   },
   {
-    "revision": "3b434e9e4e37b0d656fc",
+    "revision": "da2a34934418b2092413",
     "url": "/build/app.css"
   },
   {
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/build/precache-manifest.8f64852aab0c9c6866cbf09c305e2610.js", "https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
+importScripts("/build/precache-manifest.47ed3fef402ed2123373bc01bfa73e7c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
 
 /* global  importScripts, workbox*/
 
