2fc5f41a1fd3e801245584a85b4647aede7f26cd
muneneevans <munene.evansk@gmail.com>
2018-05-30 11:31:35 +0300

    [BUILD]

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/build/precache-manifest.6e8c311f2eb36502004ae0e8523da99b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
+importScripts("/build/precache-manifest.cdfb5fb3f58091f7772eea69fe47fc99.js", "https://storage.googleapis.com/workbox-cdn/releases/3.0.1/workbox-sw.js");
 
 /* global  importScripts, workbox*/
 
