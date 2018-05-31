68fee1289e90f3fb82b23f6432c1edd85f6b2ec8
muneneevans <munene.evansk@gmail.com>
2018-05-31 23:10:47 +0300

    [BUILD]

--- public/build/precache-manifest.41347861c2acf8440fc240e9e590480f.js
+++ public/build/precache-manifest.41347861c2acf8440fc240e9e590480f.js
@@ -17,7 +17,7 @@ self.__precacheManifest = [
     "url": "/build/f2a4eeab3c5e6b50c5d9f1e65b36518a.png"
   },
   {
-    "revision": "0796815c4e1866fc9b4f",
+    "revision": "99ff21257314405ea846",
     "url": "/build/app.js"
   },
   {
@@ -42,7 +42,7 @@ self.__precacheManifest = [
     "url": "/build/f4043e726fb5d48655db2f5ecff8d74c.woff2"
   },
   {
-    "revision": "0796815c4e1866fc9b4f",
+    "revision": "99ff21257314405ea846",
     "url": "/build/app.css"
   },
   {
